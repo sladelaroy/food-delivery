f417d36f25a428bb4c04047e82e4e9bcf353a275
sladelaroy <fuunshofaleye1@gmail.com>
2025-02-13 08:35:50 +0100

    second commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+import { assets } from "../assets/assets.js";
+const Navbar = () => {
+
+  const [menu, setMenu] = useState("home")
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={() => setMenu('home')} className={menu === 'home' ? 'active' : ''}>home</li>
+        <li onClick={() => setMenu('menu')} className={menu === 'menu' ? 'active' : ''}>menu</li>
+        <li onClick={() => setMenu('mobile-active')} className={menu === 'mobile-active' ? 'active' : ''}>mobile-app</li>
+        <li onClick={() => setMenu('contact-us')} className={menu === 'contact-us' ? 'active' : ''}>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Navbar;
